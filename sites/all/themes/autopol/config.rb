577edffb7ca720d83a66d05717d0ec723f80c4a1
tao-io1 <tao@io1.co>
2016-04-08 15:17:14 +0300

    Init

---
+++ sites/all/themes/autopol/config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "i"
+javascripts_dir = "js"
+
+# Require any additional compass plugins installed on your system.
+require 'compass-normalize'
+require 'rgbapng'
+require 'toolkit'
+require 'breakpoint'
+require 'susy'
+require 'singularitygs'
+require 'sass-globbing'
